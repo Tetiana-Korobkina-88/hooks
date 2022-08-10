0756fb46b4bfd21c09282e2f6e3d418fccbb3e61
Tetiana-Korobkina-88 <kor1988tanya@gmail.com>
2022-08-10 12:46:18 +0200

    worked with useState

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { State } from "./hooks/State";
+
+function App() {
+  return (
+    <div className="App">
+      <State />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/State.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+export const State = () => {
+  const [state, setState] = useState({
+    count: 0,
+    isCounting: false,
+  });
+  // const [value, setValue] = useState(() => {
+  //   const userCount = localStorage.getItem("count");
+  //   return +userCount || 0;
+  // });
+
+  // setValue((prevValue) => {
+  //   return prevValue + 1;
+  // });
+
+  const handleCount = () => {
+    setState({ ...state, count: state.count + 1 });
+  };
+
+  const handleStatus = () => {
+    setState({ ...state, isCounting: !state.isCounting });
+  };
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  return (
+    <div>
+      <button onClick={handleCount}>click</button>
+      <button onClick={handleStatus}>me too</button>
+    </div>
+  );
+};
